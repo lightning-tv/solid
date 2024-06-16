48f41df3c683b08bc24cabb76778953aaca92533
Chris Lorenzo <chiefcll@gmail.com>
2024-06-15 22:18:15 -0400

    remove exports for padding and sprite

--- src/primitives/index.ts
+++ src/primitives/index.ts
@@ -1,7 +1,5 @@
 export * from './useFocusManager.js';
-export * from './withPadding.js';
 export * from './announcer/index.js';
 export * from './createInfiniteItems.js';
-export * from './createSpriteMap.js';
 export * from './useMouse.js';
 export * from './portal.jsx';
