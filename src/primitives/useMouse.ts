f9dab6c4c2387b11a3ba6795b23cd1493f38a564
Chris Lorenzo <chiefcll@gmail.com>
2024-07-09 22:50:30 -0400

    update to renderer rc

--- src/primitives/useMouse.ts
+++ src/primitives/useMouse.ts
@@ -11,13 +11,6 @@ import { useMousePosition } from '@solid-primitives/mouse';
 import { createScheduled, throttle } from '@solid-primitives/scheduled';
 import { createEffect } from 'solid-js';
 
-interface MainOnlyNode extends INode {
-  coreNode: {
-    absX: number;
-    absY: number;
-  };
-}
-
 function createKeyboardEvent(key: string, keyCode: number): KeyboardEvent {
   return new KeyboardEvent('keydown', {
     key,
@@ -48,8 +41,8 @@ const handleClick = (e: MouseEvent): void => {
     testCollision(
       e.clientX,
       e.clientY,
-      (active.lng as MainOnlyNode).coreNode.absX * precision,
-      (active.lng as MainOnlyNode).coreNode.absY * precision,
+      (active.lng as INode).absX * precision,
+      (active.lng as INode).absY * precision,
       active.width! * precision,
       active.height! * precision,
     )
@@ -90,8 +83,8 @@ function getChildrenByPosition(
         testCollision(
           x,
           y,
-          (currentNode.lng as MainOnlyNode).coreNode.absX * precision,
-          (currentNode.lng as MainOnlyNode).coreNode.absY * precision,
+          (currentNode.lng as INode).absX * precision,
+          (currentNode.lng as INode).absY * precision,
           currentNode.width! * precision,
           currentNode.height! * precision,
         )
--- src/render.ts
+++ src/render.ts
@@ -13,7 +13,7 @@ import {
   type JSXElement,
   type ValidComponent,
 } from 'solid-js';
-import type { RendererMain, RendererMainSettings } from '@lightningjs/renderer';
+import type { RendererMain } from '@lightningjs/renderer';
 import { SolidNode } from './types.js';
 
 const solidRenderer = createRenderer<SolidNode>(nodeOpts);
@@ -21,36 +21,13 @@ const solidRenderer = createRenderer<SolidNode>(nodeOpts);
 let renderer: RendererMain;
 export const rootNode = nodeOpts.createElement('App');
 
-export async function startLightning(
-  options?: Partial<RendererMainSettings>,
-  rootId?: string | HTMLElement,
-) {
-  renderer = startLightningRenderer(
-    options || Config.rendererOptions,
-    rootId || 'app',
-  );
-  return await renderer.init();
-}
-
 export const render = async function (
   code: () => JSXElement,
   node?: HTMLElement | string,
 ) {
-  await startLightning(undefined, node);
+  renderer = startLightningRenderer(Config.rendererOptions, node || 'app');
   rootNode.lng = renderer.root!;
   rootNode.rendered = true;
-  // @ts-expect-error - code is jsx element and not SolidElement yet
-  const dispose = solidRenderer.render(code, rootNode);
-  return {
-    dispose,
-    rootNode,
-    renderer,
-  };
-};
-
-// used for playground - must be sync so user must await startLightning
-export const renderSync = function (code: () => JSXElement) {
-  rootNode.lng = renderer.root!;
   // @ts-expect-error - code is jsx element and not SolidElement yet
   return solidRenderer.render(code, rootNode);
 };
