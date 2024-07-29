963cc98437caad49a4b63bd4c612328fb6e0f484
Chris Lorenzo <chiefcll@gmail.com>
2024-07-29 12:31:52 -0400

    release update core

--- src/primitives/index.ts
+++ src/primitives/index.ts
@@ -4,3 +4,4 @@ export * from './createInfiniteItems.js';
 export * from './useMouse.js';
 export * from './portal.jsx';
 export * from './LazyUp.jsx';
+export type { KeyHandler } from '@lightningtv/core';
