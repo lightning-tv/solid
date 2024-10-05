06a2f51ff792f0a01b4af897dbae05b997067d0a
Chris Lorenzo <chiefcll@gmail.com>
2024-10-05 10:38:13 -0400

    update core

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-import type { NodeProps, TextProps, ElementNode } from '@lightningtv/core';
+import type { NodeProps, TextProps } from '@lightningtv/core';
 
 declare module 'solid-js' {
   namespace JSX {
