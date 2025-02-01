ac3dc1ce9f2c6b8dc9e3ec09b7ef6a89f7f7033d
Chris Lorenzo <chiefcll@gmail.com>
2025-02-01 13:35:35 -0500

    fix up portal to work correctly

--- src/primitives/portal.tsx
+++ src/primitives/portal.tsx
@@ -1,11 +1,25 @@
-import { View, rootNode, type ElementNode } from '@lightningtv/solid';
-import { type JSXElement } from 'solid-js';
+import { rootNode, type ElementNode, insert } from '@lightningtv/solid';
+import { createEffect, createMemo, createRoot, createSignal, getOwner, JSX, onCleanup, runWithOwner } from 'solid-js';
 
-export function Portal(props: { mount?: string; children: JSXElement }) {
+export function Portal(props: { mount?: string; children: JSX.Element }) {
+  let content: undefined | (() => JSX.Element);
+  const mount = () => getMount(props.mount);
+  const owner = getOwner();
+
   function getMount(mount?: string): ElementNode {
     if (!mount) return rootNode;
     return rootNode.searchChildrenById(mount) || rootNode;
   }
 
-  return <View parent={getMount(props.mount)}>{props.children}</View>;
+  createEffect(
+    () => {
+      const [clean, setClean] = createSignal(false);
+      const cleanup = () => setClean(true);
+      content || (content = runWithOwner(owner, () => createMemo(() => props.children)));
+      createRoot(dispose => insert(mount(), () => (!clean() ? content!() : dispose()), null));
+      onCleanup(cleanup);
+    }
+  );
+
+  return null;
 }
