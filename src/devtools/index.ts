7d79fdf2c7157f3f73da26091e00bf39659d859c
Damian Tarnawski <gthetarnav@gmail.com>
2025-03-31 13:31:16 +0200

    Improve devtools element interface

--- src/devtools/index.ts
+++ src/devtools/index.ts
@@ -1,28 +1,30 @@
-/*
-
- Implementation of the solid-devtools element interface for Lightning elements
-
-*/
-
 import * as debug from '@solid-devtools/debugger/types';
 import * as lng from '@lightningtv/core';
 
+const EMPTY_CHILDREN: (lng.ElementNode | lng.ElementText)[] = [];
+
+/**
+ * Implementation of the solid-devtools element interface for Lightning elements
+ */
 export const elementInterface: debug.ElementInterface<
-  lng.ElementNode | lng.ElementText | lng.TextNode
+  lng.ElementNode | lng.ElementText
 > = {
-  isElement: (node): node is lng.ElementNode | lng.ElementText | lng.TextNode =>
+  isElement: (node): node is lng.ElementNode | lng.ElementText =>
     '_type' in node &&
     (node._type === lng.NodeType.Element ||
-      node._type === lng.NodeType.TextNode ||
-      node._type === lng.NodeType.Text),
-  getChildren: (node) => node.children,
-  getName: (node) => node._type,
+      node._type === lng.NodeType.TextNode),
+  getChildren: (node) =>
+    node instanceof lng.ElementNode ? node.children : EMPTY_CHILDREN,
+  getName: (node) => (node._type === lng.NodeType.Element ? 'view' : 'text'),
   getParent: (node) => node.parent ?? null,
   getRect: (node) => {
     return null;
   },
-  getElementAt: (node) => {
-    return null;
+  getElementAt: (e) => {
+    let target = e.target as any;
+    return target != null && target.element instanceof lng.ElementNode
+      ? target.element
+      : null;
   },
   getLocation: (node) => {
     return null;
