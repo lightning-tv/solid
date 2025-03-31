3984550ec781037c7d35b6a87feaac9b52a9efb5
Damian Tarnawski <gthetarnav@gmail.com>
2025-03-31 14:04:08 +0200

    implement ElementInterface.getRect

--- src/devtools/index.ts
+++ src/devtools/index.ts
@@ -18,7 +18,33 @@ export const elementInterface: debug.ElementInterface<
   getName: (node) => (node._type === lng.NodeType.Element ? 'view' : 'text'),
   getParent: (node) => node.parent ?? null,
   getRect: (node) => {
-    return null;
+    let { width, height } = node;
+    let x = 0,
+      y = 0;
+
+    if (node.scaleX != null) width *= node.scaleX;
+    if (node.scaleY != null) height *= node.scaleY;
+
+    let curr = node as lng.ElementNode | undefined | null;
+    while (curr != null) {
+      x += curr.x;
+      y += curr.y;
+      if (curr.scaleX != null) x *= curr.scaleX;
+      if (curr.scaleY != null) y *= curr.scaleY;
+      curr = curr.parent;
+    }
+
+    if (lng.Config.rendererOptions != null) {
+      let { deviceLogicalPixelRatio } = lng.Config.rendererOptions;
+      if (deviceLogicalPixelRatio != null) {
+        x *= deviceLogicalPixelRatio;
+        y *= deviceLogicalPixelRatio;
+        width *= deviceLogicalPixelRatio;
+        height *= deviceLogicalPixelRatio;
+      }
+    }
+
+    return { x, y, width, height };
   },
   getElementAt: (e) => {
     let target = e.target as any;
