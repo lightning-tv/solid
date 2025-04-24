6b99d3ec8fbdb9530fcb2773919d22ae596830ce
Damian Tarnawski <gthetarnav@gmail.com>
2025-04-24 14:48:02 +0200

    Add CI workflow, testing setup, and initial tests for Solid components
    - Created a GitHub Actions workflow for building and testing on push and pull request events to the main branch.
    - Added VSCode launch configuration for running tests in watch mode.
    - Configured pnpm workspace to only build specific dependencies. (esbuild postinstall scripts)
    - Add test for flex when swapping parents (requires https://github.com/lightning-tv/core/pull/45)
    - Add vitest and ts config for tests to support transforming jsx with solid vite plugin.

---
+++ tests/flex.test.tsx
@@ -0,0 +1,57 @@
+import * as v from 'vitest'
+import * as s from 'solid-js'
+import * as lng from '@lightningtv/solid'
+
+import {renderer} from './setup.js'
+
+v.test('Flex with swapping parents', async () => {
+
+  const [condition, setCondition] = s.createSignal(true);
+
+  const X = 100
+
+  const one = <view width={X} height={X} />
+
+  let aContainer!: lng.ElementNode
+  let bContainer!: lng.ElementNode
+  let aTwo!: lng.ElementNode
+  let bTwo!: lng.ElementNode
+
+  const dispose = renderer.render(() => <>
+    <view ref={aContainer} display='flex'>
+      {condition() && one}
+      <view ref={aTwo} width={X} height={X} />
+    </view>
+    <view ref={bContainer} display='flex' y={X}>
+      {!condition() && one}
+      <view ref={bTwo} width={X} height={X} />
+    </view>
+  </>)
+
+  v.assert.equal(aTwo.x, X)
+  v.assert.equal(bTwo.x, 0)
+  v.assert.equal(aContainer.width, X*2)
+  v.assert.equal(bContainer.width, X)
+  v.assert.equal(aContainer.height, X)
+  v.assert.equal(bContainer.height, X)
+
+  setCondition(false)
+
+  v.assert.equal(aTwo.x, 0)
+  v.assert.equal(bTwo.x, X)
+  v.assert.equal(aContainer.width, X)
+  v.assert.equal(bContainer.width, X*2)
+  v.assert.equal(aContainer.height, X)
+  v.assert.equal(bContainer.height, X)
+
+  setCondition(true)
+
+  v.assert.equal(aTwo.x, X)
+  v.assert.equal(bTwo.x, 0)
+  v.assert.equal(aContainer.width, X*2)
+  v.assert.equal(bContainer.width, X)
+  v.assert.equal(aContainer.height, X)
+  v.assert.equal(bContainer.height, X)
+
+  dispose()
+})
