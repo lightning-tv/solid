7a5a7c838c3cfa3a54223541f8bcec3e66fc4edf
Chris Lorenzo <chiefcll@gmail.com>
2025-04-29 09:40:39 -0400

    expose Announcer and setup options

--- src/primitives/announcer/index.ts
+++ src/primitives/announcer/index.ts
@@ -2,9 +2,19 @@ import { createEffect, on } from 'solid-js';
 import { Announcer } from './announcer.js';
 import { focusPath } from '../useFocusManager.js';
 
-export const useAnnouncer = () => {
-  Announcer.setupTimers();
+let doOnce = false;
+export const useAnnouncer = (options?: {
+  focusDebounce?: number;
+  focusChangeTimeout?: number;
+}) => {
+  if (doOnce) {
+    return Announcer;
+  }
+  doOnce = true;
+  Announcer.setupTimers(options);
   createEffect(on(focusPath, Announcer.onFocusChange!, { defer: true }));
 
   return Announcer;
 };
+
+export { Announcer };
