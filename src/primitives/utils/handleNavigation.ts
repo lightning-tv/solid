6f24ae9844c8fd5f40007b49ebdb21a91bf9a19c
Damian Tarnawski <gthetarnav@gmail.com>
2025-07-10 19:58:13 +0200

    Cleanup and rename col/row navigation helpers

--- src/primitives/utils/handleNavigation.ts
+++ src/primitives/utils/handleNavigation.ts
@@ -1,113 +1,128 @@
-import { ElementNode, assertTruthy, Config } from '@lightningtv/core';
-import { type KeyHandler } from '@lightningtv/core/focusManager';
-import type { NavigableElement, OnSelectedChanged } from '../types.js';
+import * as lng from '@lightningtv/solid';
+import * as lngp from '@lightningtv/solid/primitives';
 
-export function onGridFocus(onSelectedChanged: OnSelectedChanged | undefined) {
-  return function (this: ElementNode) {
-    if (!this || this.children.length === 0) return false;
+export const navigableOnNavigation: lng.KeyHandler = function (e) {
+  return moveSelection(
+    this as lngp.NavigableElement,
+    e.key === 'ArrowUp' || e.key === 'ArrowLeft' ? -1 : 1,
+  );
+};
 
-    // only check onFocus, and not when grid.setFocus() to retrigger focus
-    if (!this.states.has(Config.focusStateKey)) {
-      // if a child already has focus, assume that should be selected
-      this.children.find((child, index) => {
-        if (child.states.has(Config.focusStateKey)) {
-          this.selected = index;
-          return true;
+export const navigableForwardFocus: lng.ForwardFocusHandler = function () {
+  if (!this || this.children.length === 0) return false;
+
+  // only check onFocus, and not when grid.setFocus() to retrigger focus
+  if (!this.states.has(lng.Config.focusStateKey)) {
+    // if a child already has focus, assume that should be selected
+    this.children.find((child, index) => {
+      if (child.states.has(lng.Config.focusStateKey)) {
+        this.selected = index;
+        return true;
+      }
+      return false;
+    });
+  }
+
+  this.selected = this.selected || 0;
+  let child = this.selected ? this.children[this.selected] : this.selectedNode;
+
+  while (child?.skipFocus) {
+    this.selected++;
+    child = this.children[this.selected];
+  }
+  if (!(child instanceof lng.ElementNode)) return false;
+  child.setFocus();
+
+  const grid = this as lngp.NavigableElement;
+  grid.onSelectedChanged?.(grid.selected, grid, child);
+
+  return true;
+};
+
+export function moveSelection(
+  el: lngp.NavigableElement,
+  delta: number,
+): boolean {
+  const numChildren = el.children.length;
+  const lastSelected = el.selected || 0;
+
+  if (numChildren === 0) {
+    return false;
+  }
+
+  if (delta > 0) {
+    do {
+      el.selected = ((el.selected || 0) % numChildren) + 1;
+      if (el.selected >= numChildren) {
+        if (!el.wrap) {
+          el.selected = -1;
+          break;
         }
-        return false;
-      });
-    }
+        el.selected = 0;
+      }
+    } while (el.children[el.selected]?.skipFocus);
+  } else if (delta < 0) {
+    do {
+      el.selected = ((el.selected || 0) % numChildren) - 1;
+      if (el.selected < 0) {
+        if (!el.wrap) {
+          el.selected = -1;
+          break;
+        }
+        el.selected = numChildren - 1;
+      }
+    } while (el.children[el.selected]?.skipFocus);
+  }
 
-    this.selected = this.selected || 0;
-    let child = this.selected
-      ? this.children[this.selected]
-      : this.selectedNode;
-
-    while (child?.skipFocus) {
-      this.selected++;
-      child = this.children[this.selected];
+  if (el.selected === -1) {
+    el.selected = lastSelected;
+    if (lng.isFocused(el)) {
+      // This child is already focused, so bubble up to next handler
+      return false;
     }
-    if (!(child instanceof ElementNode)) return false;
-    child.setFocus();
+  }
+  const active = el.children[el.selected || 0] || el.children[0];
+  if (!(active instanceof lng.ElementNode)) return false;
+  const navigableThis = el as lngp.NavigableElement;
 
-    if (onSelectedChanged) {
-      const grid = this as NavigableElement;
-      onSelectedChanged.call(grid, grid.selected, grid, child);
-    }
-    return true;
+  navigableThis.onSelectedChanged?.(
+    navigableThis.selected,
+    navigableThis,
+    active,
+    lastSelected,
+  );
+
+  if (el.plinko) {
+    // Set the next item to have the same selected index
+    // so we move up / down directly
+    const lastSelectedChild = el.children[lastSelected];
+    lng.assertTruthy(lastSelectedChild instanceof lng.ElementNode);
+    const num = lastSelectedChild.selected || 0;
+    active.selected =
+      num < active.children.length ? num : active.children.length - 1;
+  }
+  active.setFocus();
+  return true;
+}
+
+/** @deprecated Use {@link navigableForwardFocus} instead */
+export function onGridFocus(
+  onSelectedChanged: lngp.OnSelectedChanged | undefined,
+): lng.ForwardFocusHandler {
+  return function () {
+    this.onSelectedChanged = onSelectedChanged;
+    navigableForwardFocus.call(this, this);
   };
 }
 
+/** @deprecated Use {@link navigableOnNavigation} instead */
 export function handleNavigation(
   direction: 'up' | 'right' | 'down' | 'left',
-): KeyHandler {
+): lng.KeyHandler {
   return function () {
-    const numChildren = this.children.length;
-    const wrap = this.wrap;
-    const lastSelected = this.selected || 0;
-
-    if (numChildren === 0) {
-      return false;
-    }
-
-    if (direction === 'right' || direction === 'down') {
-      do {
-        this.selected = ((this.selected || 0) % numChildren) + 1;
-        if (this.selected >= numChildren) {
-          if (!wrap) {
-            this.selected = -1;
-            break;
-          }
-          this.selected = 0;
-        }
-      } while (this.children[this.selected]?.skipFocus);
-    } else if (direction === 'left' || direction === 'up') {
-      do {
-        this.selected = ((this.selected || 0) % numChildren) - 1;
-        if (this.selected < 0) {
-          if (!wrap) {
-            this.selected = -1;
-            break;
-          }
-          this.selected = numChildren - 1;
-        }
-      } while (this.children[this.selected]?.skipFocus);
-    }
-
-    if (this.selected === -1) {
-      this.selected = lastSelected;
-      if (
-        this.children[this.selected]?.states!.has(
-          Config.focusStateKey || '$focus',
-        )
-      ) {
-        // This child is already focused, so bubble up to next handler
-        return false;
-      }
-    }
-    const active = this.children[this.selected || 0] || this.children[0];
-    if (!(active instanceof ElementNode)) return false;
-    const navigableThis = this as NavigableElement;
-
-    navigableThis.onSelectedChanged &&
-      navigableThis.onSelectedChanged.call(
-        navigableThis,
-        navigableThis.selected,
-        navigableThis,
-        active,
-        lastSelected,
-      );
-
-    if (this.plinko) {
-      // Set the next item to have the same selected index
-      // so we move up / down directly
-      const lastSelectedChild = this.children[lastSelected];
-      assertTruthy(lastSelectedChild instanceof ElementNode);
-      const num = lastSelectedChild.selected || 0;
-      active.selected =
-        num < active.children.length ? num : active.children.length - 1;
-    }
-    active.setFocus();
-    return true;
+    return moveSelection(
+      this as lngp.NavigableElement,
+      direction === 'up' || direction === 'left' ? -1 : 1,
+    );
   };
 }
