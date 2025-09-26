002a0c86d76f6137d138e074532d1369f2131bd9
Chris Lorenzo <chiefcll@gmail.com>
2025-09-26 17:32:18 -0400

    working with latest beta

--- src/primitives/utils/withScrolling.ts
+++ src/primitives/utils/withScrolling.ts
@@ -64,7 +64,7 @@ export function withScrolling(isRow: boolean): Scroller {
     }
 
     const lng = componentRef.lng as unknown as INode;
-    const screenSize = isRow ? lng.stage.root.width : lng.stage.root.height;
+    const screenSize = isRow ? lng.stage.root.w : lng.stage.root.h;
     // Determine if movement is incremental or decremental
     const isIncrementing =
       lastSelected === undefined || lastSelected - 1 !== selected;
