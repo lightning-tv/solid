09b53696d2d507570b7a4e4254e17aa76eb76f7a
Chris Lorenzo <chiefcll@gmail.com>
2026-02-10 09:51:47 -0700

    add more props / simplify border

--- src/primitives/borderBox.tsx
+++ src/primitives/borderBox.tsx
@@ -4,41 +4,35 @@ import {
   type NodeProps,
   type NodeStyles,
 } from '@lightningtv/solid';
-import {
-  createMemo,
-  getOwner,
-  onMount,
-  runWithOwner,
-  type Accessor,
-} from 'solid-js';
+import { getOwner, runWithOwner, type Accessor } from 'solid-js';
 import { fadeIn, fadeOut } from './FadeInOut.jsx';
 import { chainFunctions } from '@lightningtv/solid/primitives';
 
 export const BorderBoxStyle: NodeStyles = {
-  alpha: 0,
+  alpha: 0.01,
   borderSpace: 6,
   borderRadius: 20,
   border: { color: 0xffffff, width: 2 },
+  scale: 1,
 };
 
 type BorderProps = NodeProps & { borderSpace?: number };
 const borderComponent = (props: BorderProps) => {
-  const space = createMemo(
-    () => props.borderSpace ?? (BorderBoxStyle.borderSpace as number),
-  );
+  const space = props.borderSpace ?? (BorderBoxStyle.borderSpace as number);
+  const scale = props.scale ?? (BorderBoxStyle.scale as number);
   return (
     <view
       skipFocus
       onCreate={(el) => {
         const parent = el.parent!;
-        el.width = parent.width + space() * 2;
-        el.height = parent.height + space() * 2;
+        el.width = parent.width * scale + space * 2;
+        el.height = parent.height * scale + space * 2;
         fadeIn(el);
       }}
       onDestroy={fadeOut}
       style={BorderBoxStyle}
-      x={-space()}
-      y={-space()}
+      x={-space}
+      y={-space}
       {...props}
     />
   );
@@ -52,23 +46,21 @@ export function borderBox(
   let border: ElementNode | null;
   const owner = getOwner();
 
-  onMount(() => {
-    el.onFocusChanged = chainFunctions((f) => {
-      if (f) {
-        if (border) return;
-        runWithOwner(owner, () => {
-          const props = accessor();
-          border = borderComponent(
-            props === true || props === undefined ? ({} as NodeProps) : props,
-          ) as any as ElementNode;
-          insertNode(el, border);
-          border.render();
-        });
-      } else if (border) {
-        border.destroy();
-        el.removeChild(border!);
-        border = null;
-      }
-    }, el.onFocusChanged);
-  });
+  el.onFocusChanged = chainFunctions((f) => {
+    if (f) {
+      if (border) return;
+      runWithOwner(owner, () => {
+        const props = accessor();
+        border = borderComponent(
+          props === true || props === undefined ? ({} as NodeProps) : props,
+        ) as any as ElementNode;
+        insertNode(el, border);
+        border.render();
+      });
+    } else if (border) {
+      border.destroy();
+      el.removeChild(border!);
+      border = null;
+    }
+  }, el.onFocusChanged);
 }
